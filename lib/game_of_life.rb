8bc4d3832af321d3a27a7d234fce8d4cdc38238f
Sergio Burgueño <burguer@gmail.com>
2020-12-19 16:42:16 -0800

    initial commit

---
+++ lib/game_of_life.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "game_of_life/version"
+
+module GameOfLife
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/game_of_life/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GameOfLife
+  VERSION = "0.1.0"
+end
