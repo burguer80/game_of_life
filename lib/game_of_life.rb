542832d66a3d1455befcaf1edea57e23325e849a
Sergio Burgueño <burguer@gmail.com>
2020-12-24 17:06:06 -0800

    Added the requires to the GameOfLife class

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
-
 require_relative "game_of_life/version"
-
+require_relative "game_of_life/models/grid"
+require_relative "game_of_life/models/state"
+require_relative "game_of_life/actions/next_generation"
+require_relative "game_of_life/actions/start_game"
+require_relative "game_of_life/utils/common"
+require_relative "game_of_life/views/terminal"
 module GameOfLife
-  class Error < StandardError; end
-  # Your code goes here...
+  def run
+    Actions::StartGame.new.call
+  end
 end
