047fa68c97bb40ef8b85a423475304dcb057f901
Sergio Burgueño <burguer@gmail.com>
2020-12-27 13:10:12 -0800

    updated GameOfLife.run method to be called properly

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative "game_of_life/version"
 require_relative "game_of_life/models/grid"
 require_relative "game_of_life/models/state"
@@ -6,8 +7,9 @@ require_relative "game_of_life/actions/next_generation"
 require_relative "game_of_life/actions/start_game"
 require_relative "game_of_life/utils/common"
 require_relative "game_of_life/views/terminal"
+
 module GameOfLife
-  def run
+  def self.run
     Actions::StartGame.new.call
   end
 end
